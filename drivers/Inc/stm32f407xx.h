905b4500bae404234ad75a61bab0aaf294865a51
joelikane <likanejoel@gmail.com>
2026-04-07 13:00:02 +0200

    USART send message from STM32 board to Arduino board implemented

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -392,7 +392,7 @@ typedef struct
 #define IRQ_NO_EXTI9_5			23
 #define IRQ_NO_EXTI15_10		40
 
-//macros IRQ (Tnterrupt Request) number for SPI peripherals
+//macros IRQ (Interrupt Request) number for SPI peripherals
 
 #define IRQ_NO_SPI1		35
 #define IRQ_NO_SPI2		36
